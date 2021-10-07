bdf9b9dad920df99c1b1be27e509175e3946c1ba
Jeffry Babb <ahuimanu@gmail.com>
2021-10-06 21:47:05 -0500

    updated leads templates

--- leads/views.py
+++ leads/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import random
 
+from .models import Lead
+
 # Create your views here.
 def home_page(request):
 
     number = random.randint(1,10)
+    leads_list = Lead.objects.all()
+
     context = {
-        'number': number
+        'number': number,
+        'leads_list': leads_list
     }
 
     return render(request, "leads/home_page.html", context)
