5f26782ff183f5dcf0102b763dd782641b8d85e3
Jeffry Babb <ahuimanu@gmail.com>
2021-10-06 21:31:32 -0500

    progressed to templates and context

--- leads/urls.py
+++ leads/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 from . import views
+
+app_name = "leads"
 
 urlpatterns = [
     path('', views.home_page, name='leads_home_page'),
--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 def home_page(request):
-    return render(request, "home_page.html")
+
+    number = random.randint(1,10)
+    context = {
+        'number': number
+    }
+
+    return render(request, "leads/home_page.html", context)
